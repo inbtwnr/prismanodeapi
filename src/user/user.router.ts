b01e7a40b94688452400a7dd3e3464f2bbb001c7
alexander <alexanderjuror@gmail.com>
2024-08-24 17:27:35 +0300

    added created, updating and deleting of user

--- src/user/user.router.ts
+++ src/user/user.router.ts
@@ -15,3 +15,72 @@ userRouter.get('/', async (req: Request, res: Response) => {
     res.status(500).json(error.message);
   }
 });
+
+// GET /users/:id
+userRouter.get('/:id', async (req: Request, res: Response) => {
+  const { id } = req.params;
+  const userId = parseInt(id, 10);
+  try {
+    const user = await UserService.getUser(userId);
+    if (!user) {
+      return res.status(404).json('User not found');
+    }
+    return res.status(200).json(user);
+  } catch (error: any) {
+    res.status(500).json(error.message);
+  }
+});
+
+// POST /users
+userRouter.post('/', [
+  body('name').isString().notEmpty(),
+], async (req: Request, res: Response) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
+  const { name } = req.body;
+  try {
+    const user = await UserService.createUser({ name });
+    return res.status(201).json(user);
+  } catch (error: any) {
+    res.status(500).json(error.message);
+  }
+});
+
+// PUT /users/:id
+userRouter.put('/:id', [
+  body('name').isString().notEmpty(),
+], async (req: Request, res: Response) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
+  const { id } = req.params;
+  const userId = parseInt(id, 10);
+  const { name } = req.body;
+  try {
+    const user = await UserService.updateUser(userId, { name });
+    if (!user) {
+      return res.status(404).json('User not found');
+    }
+    return res.status(200).json(user);
+  } catch (error: any) {
+    res.status(500).json(error.message);
+  }
+});
+
+// DELETE /users/:id
+userRouter.delete('/:id', async (req: Request, res: Response) => {
+  const { id } = req.params;
+  const userId = parseInt(id, 10);
+  try {
+    const user = await UserService.deleteUser(userId);
+    if (!user) {
+      return res.status(404).json('User not found');
+    }
+    return res.status(200).json(user);
+  } catch (error: any) {
+    res.status(500).json(error.message);
+  }
+});
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -14,4 +14,57 @@ const listUsers = async (): Promise<User[]>  => {
     })
 }
 
-export { listUsers }
+const getUser = async (id: number): Promise<User | null> => {
+    return db.user.findUnique({
+        where: {
+            id
+        },
+        select: {
+            id: true,
+            name: true
+        }
+    })
+}
+
+const createUser = async (user: Omit<User, 'id'>): Promise<User> => {
+    const { name } = user
+    return db.user.create({
+        data: {
+            name
+        },
+        select: {
+            id: true,
+            name: true
+        }
+    })
+}
+
+const updateUser = async (id: number, user: Omit<User, 'id'>): Promise<User | null> => {
+    const { name } = user
+    return db.user.update({
+        where: {
+            id
+        },
+        data: {
+            name
+        },
+        select: {
+            id: true,
+            name: true
+        }
+    })
+}
+
+const deleteUser = async (id: number): Promise<User | null> => {
+    return db.user.delete({
+        where: {
+            id
+        },
+        select: {
+            id: true,
+            name: true
+        }
+    })
+}
+
+export { listUsers, getUser, createUser, updateUser, deleteUser }
